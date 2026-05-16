9ee7efc8dde1212a79522838a4e60e4ce9710b8b
John Doe <johndoe@example.com>
2026-05-16 18:48:17 +0700

    nixos: update 2026-05-16_18-48-17

--- flake.nix
+++ flake.nix
@@ -2,15 +2,17 @@
 	description = "Hyprland";
 
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-			fcitx5-lotus = {
-				url = "github:justanoobcoder/fcitx5-lotus/fix-extra-cmake-modules";
-				inputs.nixpkgs.follows = "nixpkgs";
-		 };
-			gsr-ui-nix = {
-				url = "github:rPlakama/gsr-ui-nix";
-				inputs.nixpkgs.follows = "nixpkgs";
-			};
+		nixpkgs = {
+			url = "github:NixOS/nixpkgs/nixos-unstable";
+		};
+		fcitx5-lotus = {
+			url = "github:justanoobcoder/fcitx5-lotus/fix-extra-cmake-modules";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
+		gsr-ui-nix = {
+			url = "github:rPlakama/gsr-ui-nix";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 	outputs = inputs@{ self, nixpkgs, ... }: {
 		nixosConfigurations.amalthea = nixpkgs.lib.nixosSystem {
