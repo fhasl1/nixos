d0603af73e1d601f389ea0be0614b7a1880c4fa0
John Doe <johndoe@example.com>
2026-05-18 20:40:40 +0700

    nixos: update 2026-05-18_20-40-40

--- modules/obs.nix
+++ modules/obs.nix
@@ -6,7 +6,6 @@
       plugins = with pkgs.obs-studio-plugins; [
         wlrobs
         obs-pipewire-audio-capture
-        pkgs.obs-studio-plugins.obs-vkcapture
         obs-gstreamer
         obs-vaapi
       ];
