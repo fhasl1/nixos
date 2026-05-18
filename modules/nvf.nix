bc5eec11ef3b5948ff4317fb971131a321284946
John Doe <johndoe@example.com>
2026-05-18 18:00:06 +0700

    nixos: update 2026-05-18_18-00-06

--- modules/nvf.nix
+++ modules/nvf.nix
@@ -17,4 +17,29 @@
   };
   autocomplete.nvim-cmp.enable = true;
   };
+  opts = {
+    smartindent = true;
+    autoindent = true;
+    shiftwidth = 3;
+    tabstop = 3;
+    softtabstop = 3;
+    winborder = "rounded";
+    expandtab = true;
+    number = true;
+    relavtivenumber = true;
+    cursorline = true;
+    scrolloff = 8;
+    showcmd = true;
+    wrap = true;
+    linebreak = true;
+    swapfile = false;
+    backup = false;
+    clipboard = "unnamedplus";
+    hlsearch = true;
+    incsearch = true;
+    ignorecase = true;
+    smartcase = true;
+    termguicolors = true;
+    showmode = false;
+  };
 }
