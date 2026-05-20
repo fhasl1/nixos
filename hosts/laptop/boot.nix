5eaa22f8dbed97abe8e6b96e5f24741ec651c7f5
John Doe <johndoe@example.com>
2026-05-20 17:37:34 +0700

    nixos: update 2026-05-20_17-37-34

--- hosts/laptop/boot.nix
+++ hosts/laptop/boot.nix
@@ -7,7 +7,10 @@
          devices = [ "/dev/nvme0n1" ];
          configurationLimit = 5;
       };
+<<<<<<< HEAD
       kernelPackages = pkgs.linuxPackages_latest;
       kernelParams = [ "psmouse.synaptis_intertouch=1" ];
+=======
+>>>>>>> 26c3871 (nixos: update 2026-05-20_17-37-34)
    };
 }
