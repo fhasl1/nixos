b966d59e05c219ddbea686882b8983ba88d76221
John Doe <johndoe@example.com>
2026-05-31 13:36:25 +0700

    nixos: update 2026-05-31_13-36-25

--- hosts/pc/networking.nix
+++ hosts/pc/networking.nix
@@ -9,6 +9,11 @@
     nftables.enable = true;
     firewall = {
       enable = true;
+      allowedTCPPorts = [80 443];
+      allowedUDPPortRanges = [
+        { from = 4000; to = 4007; }
+        { from = 8000; to = 8010; }
+      ];
       trustedInterfaces = [ "tailscale0" ];
       allowedUDPPorts = [ config.services.tailscale.port ];
     };
