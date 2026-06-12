c7c24ace76726cd19c065d87a159964ff469b233
fhasl1 <gatkpoytxd@gmail.com>
2026-06-12 14:14:29 +0700

    nixos: update 2026-06-12_14-14-29

--- home-manager/nvim/extra.nix
+++ home-manager/nvim/extra.nix
@@ -41,80 +41,80 @@ in {
     ];
 
     extraConfigLua = ''
-    -- LSP capabilities (Neovim 0.11 built-in API)
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
-    vim.lsp.config("*", {
-      capabilities = capabilities,
-    })
+      -- LSP capabilities (Neovim 0.11 built-in API)
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+      vim.lsp.config("*", {
+        capabilities = capabilities,
+      })
 
-    -- Lua Language Server
-    vim.lsp.config("lua_ls", {
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { "vim" },
-          },
-          workspace = {
-            library = vim.api.nvim_get_runtime_file("", true),
-            checkThirdParty = false,
-          },
-        },
-      },
-    })
-    vim.lsp.enable("lua_ls")
-
-    -- clangd
-    vim.lsp.config("clangd", {
-      cmd = {
-        "clangd",
-        "--query-driver=/nix/store/*/bin/gcc*,/nix/store/*/bin/g++*,/nix/store/*/bin/clang*",
-      },
-    })
-
-    -- nixd
-    vim.lsp.config("nixd", {
-      cmd = { "nixd" },
-      capabilities = require("blink.cmp").get_lsp_capabilities(),
-      settings = {
-        nixd = {
-          nixpkgs = {
-            expr = "import <nixpkgs> { }",
-          },
-          formatting = {
-            command = { "alejandra" },
-          },
-          options = {
-            nixos = {
-              expr = '(builtins.getFlake "github:fhasl1/nixos").nixosConfigurations.amalthea.options',
+      -- Lua Language Server
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
             },
           },
         },
-      },
-    })
-    vim.lsp.enable("nixd")
+      })
+      vim.lsp.enable("lua_ls")
 
-    -- DAP setup with mason-nvim-dap
-    require("mason-nvim-dap").setup({
-      ensure_installed = { "cppdbg" },
-      automatic_installation = true,
-      handlers = {
-        function(config)
-          require("mason-nvim-dap").default_setup(config)
-        end,
-      },
-    })
+      -- clangd
+      vim.lsp.config("clangd", {
+        cmd = {
+          "clangd",
+          "--query-driver=/nix/store/*/bin/gcc*,/nix/store/*/bin/g++*,/nix/store/*/bin/clang*",
+        },
+      })
 
-    -- LuaSnip vscode loader
-    require("luasnip.loaders.from_vscode").lazy_load()
+      -- nixd
+      vim.lsp.config("nixd", {
+        cmd = { "nixd" },
+        capabilities = require("blink.cmp").get_lsp_capabilities(),
+        settings = {
+          nixd = {
+            nixpkgs = {
+              expr = "import <nixpkgs> { }",
+            },
+            formatting = {
+              command = { "alejandra" },
+            },
+            options = {
+              nixos = {
+                expr = '(builtins.getFlake "github:fhasl1/nixos").nixosConfigurations.artemis.options',
+              },
+            },
+          },
+        },
+      })
+      vim.lsp.enable("nixd")
 
-    -- Statusline
-    require("statusline").setup({
-      match_colorscheme = true,
-      tabline = true,
-      lsp_diagnostics = true,
-      ale_diagnostics = false,
-    })
+      -- DAP setup with mason-nvim-dap
+      require("mason-nvim-dap").setup({
+        ensure_installed = { "cppdbg" },
+        automatic_installation = true,
+        handlers = {
+          function(config)
+            require("mason-nvim-dap").default_setup(config)
+          end,
+        },
+      })
+
+      -- LuaSnip vscode loader
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      -- Statusline
+      require("statusline").setup({
+        match_colorscheme = true,
+        tabline = true,
+        lsp_diagnostics = true,
+        ale_diagnostics = false,
+      })
     '';
   };
 }
--- home-manager/nvim/plugins/blink-cmp.nix
+++ home-manager/nvim/plugins/blink-cmp.nix
@@ -1,28 +1,28 @@
-{ ... }: {
+{...}: {
   programs.nixvim.plugins.blink-cmp = {
     enable = true;
     settings = {
       keymap = {
-        "<C-b>" = [ "scroll_documentation_up" "fallback" ];
-        "<C-f>" = [ "scroll_documentation_down" "fallback" ];
-        "<C-Space>" = [ "show" "fallback" ];
-        "<C-e>" = [ "hide" "fallback" ];
-        "<CR>" = [ "accept" "fallback" ];
+        "<C-b>" = ["scroll_documentation_up" "fallback"];
+        "<C-f>" = ["scroll_documentation_down" "fallback"];
+        "<C-Space>" = ["show" "fallback"];
+        "<C-e>" = ["hide" "fallback"];
+        "<CR>" = ["accept" "fallback"];
       };
       appearance = {
-        use_nvim_cmp_as_default = true;
+        use_nvim_cmp_as_default = false;
         nerd_font_variant = "mono";
       };
       sources = {
-        default = [ "lsp" "path" "snippets" "buffer" ];
+        default = ["lsp" "path" "snippets" "buffer"];
       };
       completion = {
-        documentation = { auto_show = true; };
+        documentation = {auto_show = true;};
       };
       cmdline = {
         keymap = {
-          "<Tab>" = [ "show" "accept" ];
-          "<CR>" = [ "accept_and_enter" "fallback" ];
+          "<Tab>" = ["show" "accept"];
+          "<CR>" = ["accept_and_enter" "fallback"];
         };
         completion = {
           menu = {
