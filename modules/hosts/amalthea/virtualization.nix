db7291ee648569033114320f5b87b78ee6f905b5
fhasl1 <gatkpoytxd@gmail.com>
2026-06-17 06:57:04 +0700

    nixos: update 2026-06-17_06-57-04

--- modules/hosts/amalthea/virtualization.nix
+++ modules/hosts/amalthea/virtualization.nix
@@ -6,8 +6,27 @@
     };
   };
   virtualisation = {
-    libvirtd.enable = true;
+    libvirtd = {
+      enable = true;
+    };
     spiceUSBRedirection.enable = true;
   };
   users.groups.libvirtd.members = ["fhasl"];
+  boot = {
+    kernelModules = ["tun" "vhost_net"];
+    kernel.sysctl = {
+      "net.ipv4.ip_forward" = 1;
+    };
+  };
+  networking = {
+    nat = {
+      enable = true;
+      externalInterface = "enp6s0";
+      internalInterfaces = ["virbr0"];
+    };
+    firewall = {
+      enable = true;
+      trustedInterfaces = ["virbr0"];
+    };
+  };
 }
