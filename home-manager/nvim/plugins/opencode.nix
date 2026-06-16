54c8c83dd35a7e0b7dbc7a0a1d535f572584a6d3
John Doe <johndoe@example.com>
2026-06-16 17:24:57 +0700

    nixos: update 2026-06-16_17-24-57

--- home-manager/nvim/plugins/opencode.nix
+++ home-manager/nvim/plugins/opencode.nix
@@ -1,5 +1,55 @@
 { ... }: {
-  programs.nixvim.plugins.opencode = {
-    enable = true;
+  programs.nixvim = {
+    dependencies.opencode.enable = true;
+
+    plugins.opencode = {
+      enable = true;
+      autoLoad = true;
+    };
+
+    keymaps = [
+      {
+        mode = [ "n" "x" ];
+        key = "<C-a>";
+        action.__raw = "function() require('opencode').ask('@this: ', { submit = true }) end";
+        options = { desc = "Ask opencode"; };
+      }
+      {
+        mode = [ "n" "x" ];
+        key = "<C-x>";
+        action.__raw = "function() require('opencode').select() end";
+        options = { desc = "Execute opencode action"; };
+      }
+      {
+        mode = [ "n" "t" ];
+        key = "<C-.>";
+        action.__raw = "function() require('opencode').toggle() end";
+        options = { desc = "Toggle opencode"; };
+      }
+      {
+        mode = [ "n" "x" ];
+        key = "go";
+        action.__raw = "function() return require('opencode').operator('@this ') end";
+        options = { desc = "Add range to opencode"; expr = true; };
+      }
+      {
+        mode = "n";
+        key = "goo";
+        action.__raw = "function() return require('opencode').operator('@this ') .. '_' end";
+        options = { desc = "Add line to opencode"; expr = true; };
+      }
+      {
+        mode = "n";
+        key = "<S-C-u>";
+        action.__raw = "function() require('opencode').command('session.half.page.up') end";
+        options = { desc = "Scroll opencode up"; };
+      }
+      {
+        mode = "n";
+        key = "<S-C-d>";
+        action.__raw = "function() require('opencode').command('session.half.page.down') end";
+        options = { desc = "Scroll opencode down"; };
+      }
+    ];
   };
 }
