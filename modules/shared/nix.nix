32c902d6798d75533990033e6ff9b715c56846d4
fhasl1 <gatkpoytxd@gmail.com>
2026-06-20 07:32:11 +0700

    nixos: update 2026-06-20_07-32-11

--- modules/shared/nix.nix
+++ modules/shared/nix.nix
@@ -24,8 +24,12 @@
         "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://cache.nixos.org"
         "https://aseipp-nix-cache.global.ssl.fastly.net"
+        "https://attic.xuyh0120.win/lantian"
       ];
-      trusted-public-keys = ["cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+      ];
     };
   };
 }
