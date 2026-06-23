0a19ae77fd7fa5f66678cfaf36a07689141587e7
fhasl1 <gatkpoytxd@gmail.com>
2026-06-23 13:10:17 +0700

    nixos: update 2026-06-23_13-10-17

--- home-manager/nvim/plugins/misc.nix
+++ home-manager/nvim/plugins/misc.nix
@@ -28,5 +28,30 @@
     };
 
     friendly-snippets.enable = true;
+
+    telescope = {
+      enable = true;
+      extensions.fzf-native = {
+        enable = true;
+        settings = {};
+      };
+      keymaps = {
+        "<leader>ff" = "find_files";
+        "<leader>fg" = "live_grep";
+        "<leader>fb" = "buffers";
+        "<leader>fh" = "help_tags";
+        "<leader>fs" = "lsp_document_symbols";
+      };
+      settings = {
+        defaults = {
+          layout_strategy = "horizontal";
+          layout_config = {
+            prompt_position = "top";
+          };
+          sorting_strategy = "ascending";
+          winblend = 0;
+        };
+      };
+    };
   };
 }
