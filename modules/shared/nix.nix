de142cd03c398d88d1e5da7eab0796e80d52f89d
fhasl1 <gatkpoytxd@gmail.com>
2026-07-09 07:00:41 +0700

    nixos: update 2026-07-09_07-00-41

--- modules/shared/nix.nix
+++ modules/shared/nix.nix
@@ -12,9 +12,11 @@
         "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://cache.nixos.org"
         "https://aseipp-nix-cache.global.ssl.fastly.net"
+        "https://cache.xinux.uz"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0="
       ];
     };
 
