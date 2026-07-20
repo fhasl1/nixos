5fd077399512c051fba168a206ebbee34ac3bb43
fhasl1 <gatkpoytxd@gmail.com>
2026-07-20 21:10:33 +0700

    nixos: update 2026-07-20_21-10-33

---
+++ modules/hosts/amalthea/emacs.nix
@@ -0,0 +1,14 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  services = {
+    emacs = {
+      enable = true;
+      package = pkgs.emacs;
+      startWithGraphical = config.services.xserver.enable;
+      install = false;
+    };
+  };
+}
