eead19dab7b72df5c35328f62cbd393994deb76d
fhasl1 <gatkpoytxd@gmail.com>
2026-07-24 22:55:19 +0700

    nixos: update 2026-07-24_22-55-19

--- modules/shared/packages.nix
+++ modules/shared/packages.nix
@@ -1,4 +1,80 @@
 {pkgs, ...}: {
+  fonts = {
+    fontDir.enable = true;
+    packages = with pkgs.nerd-fonts; [
+      _0xproto
+      _3270
+      adwaita-mono
+      agave
+      anonymice
+      arimo
+      atkynson-mono
+      aurulent-sans-mono
+      bigblue-terminal
+      bitstream-vera-sans-mono
+      blex-mono
+      caskaydia-cove
+      caskaydia-mono
+      code-new-roman
+      comic-shanns-mono
+      commit-mono
+      cousine
+      d2coding
+      daddy-time-mono
+      dejavu-sans-mono
+      departure-mono
+      droid-sans-mono
+      envy-code-r
+      fantasque-sans-mono
+      fira-code
+      fira-mono
+      geist-mono
+      go-mono
+      gohufont
+      hack
+      hasklug
+      heavy-data
+      hurmit
+      im-writing
+      inconsolata
+      inconsolata-go
+      inconsolata-lgc
+      intone-mono
+      iosevka
+      iosevka-term
+      iosevka-term-slab
+      jetbrains-mono
+      lekton
+      liberation
+      lilex
+      pkgs.nerd-fonts."m+"
+      martian-mono
+      meslo-lg
+      monaspace
+      monofur
+      monoid
+      mononoki
+      noto
+      open-dyslexic
+      overpass
+      profont
+      proggy-clean-tt
+      recursive-mono
+      roboto-mono
+      sauce-code-pro
+      shure-tech-mono
+      space-mono
+      symbols-only
+      terminess-ttf
+      tinos
+      ubuntu
+      ubuntu-mono
+      ubuntu-sans
+      victor-mono
+      zed-mono
+    ];
+  };
+
   environment.systemPackages = with pkgs; [
     opencode
 
