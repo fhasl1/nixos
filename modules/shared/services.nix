8901db1f8e83cfcadfebb0b468c340dafe4c70b0
fhasl1 <gatkpoytxd@gmail.com>
2026-07-24 08:58:43 +0700

    nixos: update 2026-07-24_08-58-43

--- modules/shared/services.nix
+++ modules/shared/services.nix
@@ -10,6 +10,15 @@
     };
     usbmuxd.enable = true;
   };
+  systemd.user.services.startpage-server = {
+    description = "Startpage HTTP Server";
+    wantedBy = ["default.target"];
+    serviceConfig = {
+      WorkingDirectory = "/home/fhasl/nixos/home-manager/config";
+      ExecStart = "${pkgs.python3}/bin/python3 -m http.server 8000";
+      Restart = "always";
+    };
+  };
   systemd.services.tailscaled.serviceConfig.Environment = [
     "TS_DEBUG_FIREWALL_MODE=nftables"
   ];
