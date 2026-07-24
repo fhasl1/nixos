04424b406e006daa8c940f83f98b85d807e17a0e
fhasl1 <gatkpoytxd@gmail.com>
2026-07-24 21:37:09 +0700

    nixos: update 2026-07-24_21-37-09

--- modules/hosts/artemis/emacs.nix
+++ modules/hosts/artemis/emacs.nix
@@ -7,57 +7,58 @@
     emacs = {
       enable = true;
       package = pkgs.emacs;
-      startWithGraphical = config.services.xserver.enable;
       install = false;
     };
   };
-  environment.systemPackages = (with pkgs; [ emacs-all-the-icons-fonts nil ]) ++ (with pkgs.emacsPackages; [
-    all-the-icons
-    company
-    company-box
-    consult
-    counsel
-    dap-mode
-    doom-modeline
-    doom-themes
-    evil
-    flycheck
-    helm
-    ivy
-    ivy-rich
-    lsp-mode
-    lsp-treemacs
-    lsp-ui
-    lsp-ivy
-    marginalia
-    nerd-icons
-    nerd-icons-dired
-    posframe
-    rainbow-delimiters
-    smartparens
-    treemacs
-    use-package
-    which-key
-    which-key-posframe
-    writegood-mode
-    yaml-mode
-    json-mode
-    toml-mode
-    dockerfile-mode
-    lua-mode
-    rustic
-    typescript-mode
-    web-mode
-    markdown-mode
-    nix-mode
-    auctex
-    helm-lsp
-    consult
-    consult-lsp
-    corfu
-    cape
-    orderless
-    vertico
-    embark
-  ]);
+  environment.systemPackages =
+    (with pkgs; [emacs-all-the-icons-fonts nil])
+    ++ (with pkgs.emacsPackages; [
+      all-the-icons
+      company
+      company-box
+      consult
+      counsel
+      dap-mode
+      doom-modeline
+      doom-themes
+      evil
+      flycheck
+      helm
+      ivy
+      ivy-rich
+      lsp-mode
+      lsp-treemacs
+      lsp-ui
+      lsp-ivy
+      marginalia
+      nerd-icons
+      nerd-icons-dired
+      posframe
+      rainbow-delimiters
+      smartparens
+      treemacs
+      use-package
+      which-key
+      which-key-posframe
+      writegood-mode
+      yaml-mode
+      json-mode
+      toml-mode
+      dockerfile-mode
+      lua-mode
+      rustic
+      typescript-mode
+      web-mode
+      markdown-mode
+      nix-mode
+      auctex
+      helm-lsp
+      consult
+      consult-lsp
+      corfu
+      cape
+      orderless
+      vertico
+      embark
+    ]);
 }
