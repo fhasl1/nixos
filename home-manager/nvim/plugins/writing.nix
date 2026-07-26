4af80baed01110ea5d6bb23e4a5c250285d8e187
fhasl1 <gatkpoytxd@gmail.com>
2026-07-26 22:48:12 +0700

    nixos: update 2026-07-26_22-48-12

--- home-manager/nvim/plugins/writing.nix
+++ home-manager/nvim/plugins/writing.nix
@@ -39,8 +39,6 @@
         callback = function()
           vim.opt_local.wrap = true
           vim.opt_local.linebreak = true
-          vim.opt_local.spell = true
-          vim.opt_local.spelllang = "en_us"
           vim.opt_local.conceallevel = 2
         end,
       })
